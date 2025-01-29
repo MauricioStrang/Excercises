14b04ffbbfaf622638df5073f312dbdb698080d0
MauricioStrang <strangmauricio@gmail.com>
2025-01-29 11:56:26 -0300

    still learning syntaxis and strings

--- kata.py
+++ kata.py
@@ -2,3 +2,20 @@
 
 def boolean_to_string(b):
     return "True" if b else "False"
+
+
+"#2. Still learning Syntaxis"
+"Create a function which translates a given DNA string into RNA."
+
+def dna_to_rna(dna):
+    rna = []
+    for x in dna:
+        if x == "T":
+            rna.append("U")
+        else:
+            rna.append(x)
+    rna_string = "".join(rna)
+    return rna_string
+
+
+dna_to_rna("TTGACCGCCGCC")            
