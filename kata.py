0680793cb10154c76bc966bf5be8928248f2bc95
MauricioStrang <strangmauricio@gmail.com>
2025-05-05 00:43:31 -0300

    out of time range

--- kata.py
+++ kata.py
@@ -814,4 +814,14 @@ def duplicate_count(text):
         if counts[char] > 1:
             duplicates +=1
     return duplicates
-print(duplicate_count('indivisibility'))
+
+
+def quarter_of(month):
+    if month in range(1, 4):
+        return 1
+    elif month in range(4, 7):
+        return 2
+    elif month in range(7, 10):
+        return 3
+    elif month in range(10, 13):
+        return 4
